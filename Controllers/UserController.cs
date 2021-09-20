009b73e5944aebd67ce545249ffe90c0f17f4d29
Brandon Lacy <blacy15512gmail.com>
2021-09-20 14:03:43 -0500

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Clinician_View_Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Clinician_View_Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Authenticate")]
+        public IActionResult Authenticate([FromBody] UserCredentials userCred)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Clinician_View_Server.Models
+{
+    public class UserCredentials
+    {
+        public string Username { get; set; }
+        public string ShaPassword { get; set; }
+    }
+}
